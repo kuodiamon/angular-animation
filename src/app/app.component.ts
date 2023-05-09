394aa4002548c85832d7310db96b32cd4edf6f35
SYSTEX\2100367M <2100367M@misystex.com>
2023-05-09 17:29:10 +0800

    use  stagger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {
   group,
   keyframes,
   query,
+  stagger,
   state,
   style,
   transition,
@@ -35,41 +36,48 @@ import { Component } from '@angular/core';
       // 用 transition 定義狀態轉換時，元件該怎麼動作
       transition('open => close', [animate('3s')]),
       transition('* => open', [
-        group([
-          animate(
-            '3s',
-            keyframes([
-              style({
-                backgroundColor: 'yellow',
-                transform: 'translateX(0px)',
-              }),
-              style({
-                backgroundColor: 'purple',
-                transform: 'translateX(50px)',
-              }),
-              style({
-                backgroundColor: 'pink',
-                transform: 'translateX(100px)',
-              }),
-              style({
-                backgroundColor: 'green',
-                transform: 'translateX(50px)',
-              }),
-              style({
-                backgroundColor: 'red',
-                transform: 'translateX(0px)',
-              }),
-            ])
-          ),
-          query('p', [
-            animate('1s', style({ color: 'white' }))
+        animate(
+          '3s',
+          keyframes([
+            style({
+              backgroundColor: 'yellow',
+              transform: 'translateX(0px)',
+            }),
+            style({
+              backgroundColor: 'purple',
+              transform: 'translateX(50px)',
+            }),
+            style({
+              backgroundColor: 'pink',
+              transform: 'translateX(100px)',
+            }),
+            style({
+              backgroundColor: 'green',
+              transform: 'translateX(50px)',
+            }),
+            style({
+              backgroundColor: 'red',
+              transform: 'translateX(0px)',
+            }),
           ])
-        ]),
+        ),
+        query('p', [
+          animate('1s', style({ color: 'white' }))
+        ])
+        // group([
+
+
+        // ]),
       ]),
     ]),
     trigger('myTrigger', [
-      transition(':leave', [
-        animate('2s', style({ transform: 'translateX(100px)', opacity: 0 })),
+      transition(':enter', [
+        query('li', [
+          style({opacity: 0}),
+          stagger(1000, [
+            animate('500ms', style({ opacity: 1 }))
+          ])
+        ])
       ]),
     ]),
   ],
