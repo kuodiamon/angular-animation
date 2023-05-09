d22b828bb740dd4b09cba79bad52ef94fee166f7
SYSTEX\2100367M <2100367M@misystex.com>
2023-05-09 17:28:54 +0800

    use  query

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import {
   animate,
+  group,
   keyframes,
+  query,
   state,
   style,
   transition,
@@ -18,7 +20,7 @@ import { Component } from '@angular/core';
       state(
         'open',
         style({
-          backgroundColor: 'blue',
+          backgroundColor: '#ffffff',
         })
       ),
 
@@ -33,31 +35,36 @@ import { Component } from '@angular/core';
       // 用 transition 定義狀態轉換時，元件該怎麼動作
       transition('open => close', [animate('3s')]),
       transition('* => open', [
-        animate(
-          '3s',
-          keyframes([
-            style({
-              backgroundColor: 'yellow',
-              transform: 'translateX(0px)',
-            }),
-            style({
-              backgroundColor: 'purple',
-              transform: 'translateX(50px)',
-            }),
-            style({
-              backgroundColor: 'pink',
-              transform: 'translateX(100px)',
-            }),
-            style({
-              backgroundColor: 'green',
-              transform: 'translateX(50px)'
-            }),
-            style({
-              backgroundColor: 'red',
-              transform: 'translateX(0px)'
-            }),
+        group([
+          animate(
+            '3s',
+            keyframes([
+              style({
+                backgroundColor: 'yellow',
+                transform: 'translateX(0px)',
+              }),
+              style({
+                backgroundColor: 'purple',
+                transform: 'translateX(50px)',
+              }),
+              style({
+                backgroundColor: 'pink',
+                transform: 'translateX(100px)',
+              }),
+              style({
+                backgroundColor: 'green',
+                transform: 'translateX(50px)',
+              }),
+              style({
+                backgroundColor: 'red',
+                transform: 'translateX(0px)',
+              }),
+            ])
+          ),
+          query('p', [
+            animate('1s', style({ color: 'white' }))
           ])
-        ),
+        ]),
       ]),
     ]),
     trigger('myTrigger', [
