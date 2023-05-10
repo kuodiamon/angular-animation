8b83203d134c8e3d00f90fdb4a3c7702a54fdc8b
SYSTEX\2100367M <2100367M@misystex.com>
2023-05-10 09:46:15 +0800

    use  useAnimation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {
   animate,
+  animation,
   group,
   keyframes,
   query,
@@ -8,8 +9,18 @@ import {
   style,
   transition,
   trigger,
+  useAnimation,
 } from '@angular/animations';
 import { Component } from '@angular/core';
+
+export const transAnimation = animation([
+  style({
+    width: '{{ width }}',
+    // height: '{{ height }}',
+    opacity: '{{ opacity }}',
+  }),
+  animate('{{ time }}'),
+]);
 
 @Component({
   selector: 'app-root',
@@ -21,7 +32,7 @@ import { Component } from '@angular/core';
       state(
         'open',
         style({
-          backgroundColor: '#ffffff',
+          backgroundColor: '#aaa',
         })
       ),
 
@@ -34,50 +45,54 @@ import { Component } from '@angular/core';
       ),
 
       // 用 transition 定義狀態轉換時，元件該怎麼動作
-      transition('open => close', [animate('3s')]),
+      transition('open => close', [
+        useAnimation(transAnimation, {
+          params: {
+            width: 0,
+            opacity: 1,
+            time: '1s',
+          },
+        }),
+      ]),
       transition('* => open', [
         animate(
           '3s',
           keyframes([
             style({
               backgroundColor: 'yellow',
-              transform: 'translateX(0px)',
+              transform: 'translateX(0px) rotate(72deg)',
+              borderRadius: '10%',
             }),
             style({
               backgroundColor: 'purple',
-              transform: 'translateX(50px)',
+              transform: 'translateX(50px) rotate(144deg)',
+              borderRadius: '25%',
             }),
             style({
               backgroundColor: 'pink',
-              transform: 'translateX(100px)',
+              transform: 'translateX(100px) rotate(216deg)',
+              borderRadius: '50%',
             }),
             style({
               backgroundColor: 'green',
-              transform: 'translateX(50px)',
+              transform: 'translateX(50px)  rotate(288deg)',
+              borderRadius: '25%',
             }),
             style({
               backgroundColor: 'red',
-              transform: 'translateX(0px)',
+              transform: 'translateX(0px) rotate(360deg)',
+              borderRadius: '10%',
             }),
           ])
         ),
-        query('p', [
-          animate('1s', style({ color: 'white' }))
-        ])
-        // group([
-
-
-        // ]),
       ]),
     ]),
     trigger('myTrigger', [
       transition(':enter', [
         query('li', [
-          style({opacity: 0}),
-          stagger(1000, [
-            animate('500ms', style({ opacity: 1 }))
-          ])
-        ])
+          style({ opacity: 0 }),
+          stagger(1000, [animate('500ms', style({ opacity: 1 }))]),
+        ]),
       ]),
     ]),
   ],
